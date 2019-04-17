2594f1fa406a8964ff540d63088d8520b01cf3b7
Dejota Freitas <djxpgs@hotmail.com>
2019-04-17 10:11:42 -0300

    Initial commit

---
+++ 0-basic/main.dart
@@ -0,0 +1,13 @@
+// COMENTARIO
+void main() {
+  print("HELLO WORLD DART");
+
+  //variaveis
+  String nome = "DEJOTA";
+  int idade = 30;
+  double altura = 1.74;
+  bool sexo = true;
+  print(nome +" - "+ idade.toString() +" - "+ altura.toString()+" - "+ sexo.toString());
+
+
+}
